fa3e216b009df06e5dcbb432a0e51208f80e2235
Sullyvan Boulanger <sullyvanboulanger@gmail.com>
2024-05-02 16:55:11 +0200

    feat: Implement TP04

---
+++ src/main/java/utils/TestMethodeStatic.java
@@ -0,0 +1,13 @@
+package utils;
+
+public class TestMethodeStatic {
+    public static void main(String[] args) {
+        String chaine = "12";
+        int parsedInt = Integer.parseInt(chaine);
+
+        int a = 7;
+        int b = 6;
+
+        System.out.println(Integer.max(a, b));
+    }
+}
