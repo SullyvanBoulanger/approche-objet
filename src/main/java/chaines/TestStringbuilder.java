593038a32054663474f7884ab37bc9b1220085d9
Sullyvan Boulanger <sullyvanboulanger@gmail.com>
2024-05-15 11:16:32 +0200

    feature: Implement TP16
    Measure performance of different ways to concat String

---
+++ src/main/java/chaines/TestStringbuilder.java
@@ -0,0 +1,44 @@
+package chaines;
+
+public class TestStringbuilder {
+    public static int ITERATIONS = 200_000;
+
+    public static void TestStringBuilder() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int j = 0; j < ITERATIONS; j++) {
+            stringBuilder.append(j);
+        }
+    }
+
+    public static void TestStringConcat() {
+        String string = "";
+        for (int j = 0; j < ITERATIONS; j++) {
+            string = string.concat(String.valueOf(j));
+        }
+    }
+    
+    public static void TestStringPlus() {
+        String string = "";
+        for (int j = 0; j < ITERATIONS; j++) {
+            string += j;
+        }
+    }
+
+    public static void measureExecutionTime(Runnable task) {
+        long start = System.currentTimeMillis();
+        task.run();
+        long end = System.currentTimeMillis();
+        System.out.println("Temps écoulé en millisecondes :" + (end - start));
+    }
+
+    public static void main(String[] args) {
+        System.out.println("String Plus");
+        measureExecutionTime(() -> TestStringPlus());
+
+        System.out.println("String Concat");
+        measureExecutionTime(() -> TestStringConcat());
+
+        System.out.println("String Builder");
+        measureExecutionTime(() -> TestStringBuilder());
+    }
+}
